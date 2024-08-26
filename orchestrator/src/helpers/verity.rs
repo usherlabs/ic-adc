a4170ce646bdf848a7dc833823af139274a20b55
xand6r <shuaibualexander@gmail.com>
2024-08-26 18:31:39 +0100

    disable annalysis and open tcp port on compose file

--- orchestrator/src/helpers/verity.rs
+++ orchestrator/src/helpers/verity.rs
@@ -1,4 +1,4 @@
-use k256::ecdsa::SigningKey;
+// use k256::ecdsa::SigningKey;
 use verity_client::client::{AnalysisConfig, VerityClient, VerityClientConfig};
 
 use crate::config::Config;
@@ -9,20 +9,21 @@ pub const DEFAULT_ANALYSIS_URL: &str = "http://127.0.0.1:8000";
 
 pub fn get_verity_client() -> VerityClient {
     let config = Config::env();
-    let mut rng = rand::thread_rng();
-    let signing_key = SigningKey::random(&mut rng);
+    // let mut rng = rand::thread_rng();
+    // let signing_key = SigningKey::random(&mut rng);
 
     let verity_config = VerityClientConfig {
         prover_url: config.prover_url,
         prover_zmq: config.prover_zmq_url,
-        analysis: if config.analysis_url.is_some() {
-            Some(AnalysisConfig {
-                analysis_url: String::from(config.analysis_url.unwrap()),
-                signing_key,
-            })
-        } else {
-            None
-        },
+        analysis: None
+        // analysis: if config.analysis_url.is_some() {
+        //     Some(AnalysisConfig {
+        //         analysis_url: String::from(config.analysis_url.unwrap()),
+        //         signing_key,
+        //     })
+        // } else {
+        //     None
+        // },
     };
 
     VerityClient::new(verity_config)
