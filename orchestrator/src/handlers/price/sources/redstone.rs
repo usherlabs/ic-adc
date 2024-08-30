65fc7cb14db33883c8996b1ff7769b8d00cebc97
xand6r <shuaibualexander@gmail.com>
2024-08-30 23:59:06 +0100

    temporarily disable prover and notary logic

--- orchestrator/src/handlers/price/sources/redstone.rs
+++ orchestrator/src/handlers/price/sources/redstone.rs
@@ -1,13 +1,8 @@
-use std::time::Duration;
-
 use anyhow::Context;
 use anyhow::{Ok, Result};
 use serde_json::Value;
-use tokio::time::sleep;
 
-use crate::{handlers::price::traits::PricingDataSource, helpers::verity::get_verity_client};
-
-use super::PROXY_FETCH_DELAY;
+use crate::handlers::price::traits::PricingDataSource;
 
 #[derive(Debug)]
 pub struct Redstone {}
@@ -27,9 +22,10 @@ impl PricingDataSource for Redstone {
     async fn get_price(ticker: String) -> Result<f64> {
         let request_url = Self::get_url(ticker).await?;
         // Send a GET request to the API using the verity client
-        let verity_client = get_verity_client();
-        let response = verity_client.get(&request_url).send().await?.text().await?;
-        sleep(Duration::from_secs(PROXY_FETCH_DELAY)).await;
+        // TODO: enable the use of verity client after prover issue has been fixed
+        // let verity_client = get_verity_client();
+        // let response = verity_client.get(&request_url).send().await?.text().await?;
+        let response = reqwest::get(&request_url).await?.text().await?;
 
         // Parse the JSON response
         let data: Value = serde_json::from_str(&response)?;
