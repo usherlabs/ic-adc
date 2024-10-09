9d55445b6427096421702758cc90faa64e696c0e
xand6r <shuaibualexander@gmail.com>
2024-10-09 23:21:05 +0100

    update compose file and integrate analysis

--- orchestrator/src/helpers/verity.rs
+++ orchestrator/src/helpers/verity.rs
@@ -1,16 +1,15 @@
-use k256::ecdsa::SigningKey;
+use k256::SecretKey;
+use rand::rngs::OsRng;
 use verity_client::client::{AnalysisConfig, VerityClient, VerityClientConfig};
 
 use crate::config::Config;
 
 pub const DEFAULT_PROVER_URL: &str = "http://127.0.0.1:8080";
 pub const DEFAULT_PROVER_ZMQ_URL: &str = "tcp://127.0.0.1:8000";
-pub const DEFAULT_ANALYSIS_URL: &str = "http://127.0.0.1:8000";
 
 pub fn get_verity_client() -> VerityClient {
     let config = Config::env();
-    let mut rng = rand::thread_rng();
-    let signing_key = SigningKey::random(&mut rng);
+    let secret_key = SecretKey::random(&mut OsRng);
 
     let verity_config = VerityClientConfig {
         prover_url: config.prover_url,
@@ -18,7 +17,7 @@ pub fn get_verity_client() -> VerityClient {
         analysis: if config.analysis_url.is_some() {
             Some(AnalysisConfig {
                 analysis_url: String::from(config.analysis_url.unwrap()),
-                signing_key,
+                secret_key,
             })
         } else {
             None
